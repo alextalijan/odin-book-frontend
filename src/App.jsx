d422b0a822c871880b80cc9e4093d9fc5d115b0b
alextalijan <acatalijan@icloud.com>
2025-11-29 21:09:50 -0600

    feat: set up React router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './components/LoginPage/LoginPage';
 
 function App() {
-  return;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
