7c58f42a5c0bd476aea0172f5c3bfa8d4fd0c93f
alextalijan <acatalijan@icloud.com>
2025-12-01 14:49:25 -0600

    feat: create UserContext to track logged in user

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import UserContext from './contexts/UserContext';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Routes>
-    </BrowserRouter>
+    <UserContext.Provider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
