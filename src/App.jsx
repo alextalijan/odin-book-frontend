cedac05566c2580a4d11c7abd3d8c2d214bbbeba
alextalijan <acatalijan@icloud.com>
2025-12-01 15:57:58 -0600

    feat: add loading screen when accessing the page

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 import UserContext from './contexts/UserContext';
+import ProtectedRoute from './components/ProtectedRoute';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 
 function App() {
   const [user, setUser] = useState(null);
+  const [loadingUser, setLoadingUser] = useState(true);
+
+  // Check if the user has already logged In
 
   function login(user) {
     setUser(user);
@@ -16,6 +20,21 @@ function App() {
     setUser(null);
   }
 
+  // If log in status is being checked, show loading screen
+  if (loadingUser)
+    return (
+      <div className="wrapper">
+        <div className="container">
+          <span className="logo">Social Media</span>
+          <img
+            src="/public/loading-icon.png"
+            alt="loading icon"
+            className="loading-icon"
+          />
+        </div>
+      </div>
+    );
+
   return (
     <UserContext.Provider value={{ user, login }}>
       <BrowserRouter>
@@ -24,7 +43,9 @@ function App() {
           <Route path="/register" element={<RegisterPage />} />
 
           {/* Protected routes */}
-          <Route element={<ProtectedRoute />}></Route>
+          <Route element={<ProtectedRoute />}>
+            <Route path="/" />
+          </Route>
         </Routes>
       </BrowserRouter>
     </UserContext.Provider>
