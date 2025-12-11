6bec2ec841f7db5637ce8480daeead34a524c75f
alextalijan <acatalijan@icloud.com>
2025-12-10 21:12:30 -0600

    feat: conditionally show follow button if the account is not the user

--- src/components/AccountListing/AccountListing.jsx
+++ src/components/AccountListing/AccountListing.jsx
@@ -1,15 +1,17 @@
 import styles from './AccountListing.module.css';
 import getStorageUrl from '../../utils/getStorageUrl';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 
 // Components
 import UnfollowModal from '../UnfollowModal/UnfollowModal';
 import CancelRequestModal from '../CancelRequestModal/CancelRequestModal';
+import UserContext from '../../contexts/UserContext';
 
 function AccountListing({ account, refreshList }) {
   const [unfollowModal, setUnfollowModal] = useState(false);
   const [cancelRequestModal, setCancelRequestModal] = useState(false);
+  const { user } = useContext(UserContext);
 
   // Function that sends a follow request to the server
   function sendFollowRequest() {
@@ -44,27 +46,29 @@ function AccountListing({ account, refreshList }) {
         <Link to={`/users/${account.username}`} className={styles.username}>
           {account.username}
         </Link>
-        <button
-          type="button"
-          className={
-            account.isFollowed || account.requestSent
-              ? `${styles.btn} ${styles['unfollow-btn']}`
-              : `${styles.btn} ${styles['follow-btn']}`
-          }
-          onClick={
-            account.requestSent
-              ? () => setCancelRequestModal(true)
-              : account.isFollowed
-                ? () => setUnfollowModal(true)
-                : () => sendFollowRequest()
-          }
-        >
-          {account.requestSent
-            ? 'Requested'
-            : !account.isFollowed
-              ? 'Follow'
-              : 'Following'}
-        </button>
+        {account.id !== user.id && (
+          <button
+            type="button"
+            className={
+              account.isFollowed || account.requestSent
+                ? `${styles.btn} ${styles['unfollow-btn']}`
+                : `${styles.btn} ${styles['follow-btn']}`
+            }
+            onClick={
+              account.requestSent
+                ? () => setCancelRequestModal(true)
+                : account.isFollowed
+                  ? () => setUnfollowModal(true)
+                  : () => sendFollowRequest()
+            }
+          >
+            {account.requestSent
+              ? 'Requested'
+              : !account.isFollowed
+                ? 'Follow'
+                : 'Following'}
+          </button>
+        )}
       </li>
       {unfollowModal && (
         <UnfollowModal
